59fce8209f8cc83c5832f52d0e0454f4790ed8ed
CarbonDPG1 <71844196+CarbonDPG1@users.noreply.github.com>
2020-11-24 00:47:43 -0400

    Update SystemFileWatcher.cs
    Updated hashing functions to use SHA1 from Microsoft rather then pulling Process returns.
    Updated the VT links.
    Updated Windows Event Creation. (Courtesy of a coworker, Noah Clements - pointing on a typo in the command causing me issues)
    
    To do - XPATH from WinCollect (QRadar). 
    - Last API call to get results.

--- SystemFileWatcher.cs
+++ SystemFileWatcher.cs
@@ -1,10 +1,12 @@
 using System;
 using System.IO;
-using System.Management.Automation;
-using System.Management.Automation.Runspaces;
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net.Http.Headers;
 
-namespace DirWatcher
+
+namespace Program
 {
     class Program
     {
@@ -20,10 +22,11 @@ namespace DirWatcher
              * However, while the API is marked as "Approved", attempts at utilizing the new API have failed. 
              * Microsoft's current API Documentation can be found here: https://docs.microsoft.com/en-us/dotnet/api/system.io.filesystemwatcher.filter?view=netcore-3.1
             */
+            Console.WriteLine("Application Started - Monitoring FileSystem");
             using (FileSystemWatcher watcher = new FileSystemWatcher())
             {
-                
-                string gitRepo = @"C:\Users\Michael\Desktop\DirWatch";
+
+                string gitRepo = @"C:\Users\michael.redbourne\DirWatch";
                 watcher.Path = gitRepo;
                 watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
                 watcher.Filter = "*.*"; // Highly inefficient...
@@ -65,35 +68,71 @@ namespace DirWatcher
 
             if (valid)
             {
-                // Get file hash
-                /*
-                PowerShell shell = PowerShell.Create();
-                shell.AddCommand("get-filehash");
-                shell.AddParameter("Path", e.FullPath);
-                shell.AddParameter("Algorithm", "SHA1");
-                var results = shell.Invoke();
-                */
 
-                Process powershell = new Process();
-                powershell.StartInfo.FileName = "powershell.exe";
-                powershell.StartInfo.Arguments = "Get-FileHash -Algorithm SHA1 -Path " + "\'" + e.FullPath + "\'";
-                powershell.Start();
-                
+                StringBuilder format;
+                using (FileStream fs = new FileStream(e.FullPath, FileMode.Open))
+                using (BufferedStream bs = new BufferedStream(fs)) {
+                    using (SHA1Managed sha1 = new SHA1Managed()) {
+                        byte[] hash = sha1.ComputeHash(bs);
+                        format = new StringBuilder(2 * hash.Length);
+                        foreach (byte b in hash) {
+                            format.AppendFormat("{0:X2}", b);
+                        }
+                    }
+                }
+                Console.WriteLine(format);
+
                 Process VTScan = new Process();
                 VTScan.StartInfo.UseShellExecute = false;
                 VTScan.StartInfo.RedirectStandardOutput = true;
-                Console.WriteLine("Stopped here?");
-                Console.Out.Flush();
+                VTScan.StartInfo.CreateNoWindow = true;
                 VTScan.StartInfo.FileName = "powershell.exe";
-                Console.WriteLine("CMD Started");
-                Console.Out.Flush();
                 VTScan.StartInfo.Arguments = "cmd /c curl --URL \"https://www.virustotal.com/vtapi/v2/file/scan\" --form \"apikey=fdf825c37d11a14426bf70d9eb0c20400fcf76dc03c4fe827e2234cb8f38bfc4\" --form \"file=@" + e.FullPath + "\"";
-                Console.WriteLine("Args loaded");
-                Console.Out.Flush();
-                VTScan.Start();
-                Console.WriteLine("POST Request sent");
-                Console.Out.Flush();
+                Console.WriteLine("VT Executed!");
+
+                Process EventCreate = new Process();
+                EventCreate.StartInfo.UseShellExecute = false;
+                EventCreate.StartInfo.FileName = "powershell.exe";
+                //EventCreate.StartInfo.Arguments = "cmd /c \"EventCreate /T Information /ID 99 /L Application /SO CloudScanner /D \"File created in directory: \"" + e.FullPath + "\". VT Link: virustotal.com/gui/file/\"";
+                EventCreate.StartInfo.Arguments = "cmd /c \"EventCreate /T Information /ID 99 /L Application /SO CloudScanner /D \'virustotal.com/gui/file/" + format + "\'\"";
+                Console.WriteLine("cmd /c \"EventCreate /T Information /ID 99 /L Application /SO CloudScanner /D \'virustotal.com/gui/file/" + format + "\'\"");
+                EventCreate.Start();
+                Console.WriteLine("Event Created!");
                 
+                /* Handling VT Scan Output - Maybe required (probably not?) */
+                int lineCount = 0;
+                StringBuilder output = new StringBuilder();
+                VTScan.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(e.Data))
+                    {
+                        lineCount++;
+                        output.Append("\n[" + lineCount + "]: " + e.Data);
+                    }
+                });
+                VTScan.Start();
+                VTScan.BeginOutputReadLine();
+                Console.WriteLine("Command Executed");
+
+                try
+                {
+                    FileStream fs = File.Open("C:\\Users\\michael.redbourne\\temp.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.WriteLine(output);
+                    sw.Flush();
+                    sw.Close();
+                }
+                catch (IOException ioe) {
+                    Console.WriteLine(ioe);
+                }
+
+                VTScan.WaitForExit();
+                VTScan.Close();
+                
+                
+                Console.Out.Flush();
+
 
             }
             Console.Out.Flush();
